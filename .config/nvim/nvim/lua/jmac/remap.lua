f7f9ba68535715e066f195e2c5bce6cae0a6a089
jmacgrillen <macgrillen@gail.com>
2025-10-11 09:29:20 +0100

    Fixed nvim cockup

--- .config/nvim/nvim/lua/jmac/remap.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<S-l>", "<cmd>bnext<CR>zz")
-vim.keymap.set("n", "<S-h>", "<cmd>bprev<CR>zz")
-vim.keymap.set("n", "<leader>a", "ggVG", { noremap = true, silent = true, desc = "Select entire file" })
-vim.keymap.set({"v", "n"}, "<leader>y", '"+y', {noremap = true, silent = true, desc = "Yank to System Clipboard"})
